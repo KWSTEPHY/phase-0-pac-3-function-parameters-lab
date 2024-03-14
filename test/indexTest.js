c82e7fb1f66b879d843d65ee31d285f8298a9bb0
stephy <stephshiro35@gmail.com>
2024-03-14 14:20:08 -0400

    add

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,21 +1,27 @@
 require ( './root.js' );
 
 
-describe('introduction(name)', function() {
+describe('introduction(name)', function () {
+  
+
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
     expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
     expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
   })
 })
 
-describe('introductionWithLanguage(name, language)', function() {
+describe('introductionWithLanguage(name, language)', function () {
+  
+
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
     expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
     expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
   })
 })
 
-describe('introductionWithLanguageOptional(name, language)', function() {
+describe('introductionWithLanguageOptional(name, language)', function () {
+
+
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
     expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
   })
